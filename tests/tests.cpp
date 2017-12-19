d152d2ce5bc21baea85f5496b1b7eb29914ea484
Willem van Ketwich <willvk@gmail.com>
2017-12-19 21:22:44 +1100

    tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,14 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "../pbemain.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+TEST_CASE( "File is loaded", "[file]" ) {
+    MainWindow *mainApp = new MainWindow();
+    const char *filename;
+    filename = "test.rom";
+	REQUIRE( 1 == 1 );
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "File is loaded with correct size", "[file]" ) {
+    REQUIRE( 1 == 1 );
 }

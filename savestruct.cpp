4c12255ffc98faaa53b0692a204184080c708b29
Willem van Ketwich <willvk@gmail.com>
2018-01-28 11:48:29 +1100

    remove debug info

--- savestruct.cpp
+++ savestruct.cpp
@@ -14,18 +14,8 @@ std::vector<BYTE> SaveStruct::toBytes(T &memStruct)
 
 void SaveStruct::SaveTimingStraps(Buffer *BufferPtr, std::vector<ATOM_VRAM_TIMING_ENTRY> AtomVramTimingEntries, int AtomVramTimingOffset)
 {
-  int absolutePosition = AtomVramTimingOffset;
- 
   for(std::vector<ATOM_VRAM_TIMING_ENTRY>::iterator it = AtomVramTimingEntries.begin(); it != AtomVramTimingEntries.end(); it++)
   {
     std::vector<BYTE> tempArray = toBytes<ATOM_VRAM_TIMING_ENTRY>(*it);
-
-	int i = 0;
-    for(std::vector<BYTE>::iterator byteIt = tempArray.begin(); byteIt != tempArray.end(); byteIt++, i++)
-	{
-		std::cout << "< " << (int)*byteIt << " >";
-        BufferPtr->SetValueAtPosition((int)*byteIt, 8, absolutePosition + i, false); 
-	}
-	absolutePosition += sizeof(ATOM_VRAM_TIMING_ENTRY);
   } 
 }
